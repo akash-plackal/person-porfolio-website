89ca8aa36d45297a75206d3a00965cf4237e6c81
akash plackal <akashplackal@gmail.com>
2022-02-04 21:59:54 +0530

    building the landing page

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { PageDiv, Title } from "./styles/LandingPage.style";
+
+const LandingPage = () => {
+  return (
+    <PageDiv>
+      <Title>Hello , I make cool websites</Title>
+    </PageDiv>
+  );
+};
+
+export default LandingPage;
