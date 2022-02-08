7f9734a7926700420ff04418838733bb02630058
akash plackal <akashplackal@gmail.com>
2022-02-08 15:35:36 +0530

    completed cards section and added animation on scroll

--- src/components/WorkSection.jsx
+++ src/components/WorkSection.jsx
@@ -9,50 +9,117 @@ import {
   ProjectDiscCard,
   ProjectDiscMain,
   ProjectDiscLink,
+  ShowMoreButton,
 } from "./styles/WorkSection.styled";
 
 import first from "../assets/cryptoverse.png";
 import second from "../assets/real-estate.png";
+import third from "../assets/weather-app.png";
+import forth from "../assets/momentum-clone.png";
 
 const WorkSection = () => {
   return (
-    <CardDiv id="work">
-      <ProjectCard>
-        <ProjectImg src={first} />
-        <ProjectDiscDiv bgColor="#fff7c7">
-          <ProjectDiscCard>
-            <ProjectDiscTech txtColor="#e29400">
-              React js / Redux / Antd
-            </ProjectDiscTech>
-            <ProjectDiscMain txtColor="#e29400">
-              A Cryptocurrency platform build using react js , redux tool tip
-              and redux.
-            </ProjectDiscMain>
-            <ProjectDiscLink txtColor="#e29400">
-              Website &#x2022; Github
-            </ProjectDiscLink>
-          </ProjectDiscCard>
-        </ProjectDiscDiv>
-      </ProjectCard>
+    <>
+      <CardDiv id="work">
+        <ProjectCard>
+          <ProjectImg
+            src={first}
+            data-aos="fade-right"
+            data-aos-delay="20"
+            data-aos-duration="1500"
+          />
+          <ProjectDiscDiv bgColor="#fff7c7">
+            <ProjectDiscCard>
+              <ProjectDiscTech txtColor="#e29400">
+                React js / Redux / Antd
+              </ProjectDiscTech>
+              <ProjectDiscMain txtColor="#e29400">
+                A Cryptocurrency platform build using react js , redux tooltip
+                and Antd.
+              </ProjectDiscMain>
+              <ProjectDiscLink txtColor="#e29400">
+                Website &#x2022; Github
+              </ProjectDiscLink>
+            </ProjectDiscCard>
+          </ProjectDiscDiv>
+        </ProjectCard>
 
-      <ProjectCard>
-        <ProjectImg src={second} />
-        <ProjectDiscDiv bgColor="#bfe0d6">
-          <ProjectDiscCard>
-            <ProjectDiscTech txtColor="#31636e">
-              React js / Redux / Antd
-            </ProjectDiscTech>
-            <ProjectDiscMain txtColor="#31636e">
-              Join a developer community, attend virtual meetups and collaborate
-              online.
-            </ProjectDiscMain>
-            <ProjectDiscLink txtColor="#31636e">
-              Website &#x2022; Github
-            </ProjectDiscLink>
-          </ProjectDiscCard>
-        </ProjectDiscDiv>
-      </ProjectCard>
-    </CardDiv>
+        <ProjectCard>
+          <ProjectImg
+            src={second}
+            data-aos="fade-left"
+            data-aos-delay="20"
+            data-aos-duration="1500"
+          />
+          <ProjectDiscDiv bgColor="#bfe0d6">
+            <ProjectDiscCard>
+              <ProjectDiscTech txtColor="#31636e">
+                React js / Redux / Antd
+              </ProjectDiscTech>
+              <ProjectDiscMain txtColor="#31636e">
+                Join a developer community, attend virtual meetups and
+                collaborate online.
+              </ProjectDiscMain>
+              <ProjectDiscLink txtColor="#31636e">
+                Website &#x2022; Github
+              </ProjectDiscLink>
+            </ProjectDiscCard>
+          </ProjectDiscDiv>
+        </ProjectCard>
+      </CardDiv>
+
+      {/* ========== */}
+
+      <CardDiv id="work">
+        <ProjectCard>
+          <ProjectImg
+            src={third}
+            data-aos="fade-right"
+            data-aos-delay="20"
+            data-aos-duration="1500"
+          />
+          <ProjectDiscDiv bgColor="#f0ebf5">
+            <ProjectDiscCard>
+              <ProjectDiscTech txtColor="#3c053c">
+                React js / Redux / Antd
+              </ProjectDiscTech>
+              <ProjectDiscMain txtColor="#3c053c">
+                A Cryptocurrency platform build using react js , redux tooltip
+                and Antd.
+              </ProjectDiscMain>
+              <ProjectDiscLink txtColor="#3c053c">
+                Website &#x2022; Github
+              </ProjectDiscLink>
+            </ProjectDiscCard>
+          </ProjectDiscDiv>
+        </ProjectCard>
+
+        <ProjectCard>
+          <ProjectImg
+            src={forth}
+            data-aos="fade-left"
+            data-aos-delay="20"
+            data-aos-duration="1500"
+          />
+          <ProjectDiscDiv bgColor="#fbd6b7">
+            <ProjectDiscCard>
+              <ProjectDiscTech txtColor="#a03516">
+                React js / Redux / Antd
+              </ProjectDiscTech>
+              <ProjectDiscMain txtColor="#a03516">
+                Join a developer community, attend virtual meetups and
+                collaborate online.
+              </ProjectDiscMain>
+              <ProjectDiscLink txtColor="#a03516">
+                Website &#x2022; Github
+              </ProjectDiscLink>
+            </ProjectDiscCard>
+          </ProjectDiscDiv>
+        </ProjectCard>
+      </CardDiv>
+
+      {/* <ShowMoreButton>Show more</ShowMoreButton> */}
+    </>
   );
 };
 
--- src/components/styles/WorkSection.styled.js
+++ src/components/styles/WorkSection.styled.js
@@ -9,30 +9,27 @@ import styled from "styled-components";
 export const CardDiv = styled.div`
   display: flex;
   justify-content: space-between;
-  /* border: 1px solid red; */
   padding: 3.5% 5%;
+  overflow: hidden;
 `;
 
 export const ProjectCard = styled.div`
   width: fit-content;
-  /* border: 1px solid brown; */
 `;
 
 export const ProjectImg = styled.img`
-  width: 600px;
+  width: 37.5rem;
   border-radius: 1.5px;
 `;
 
 export const ProjectDiscDiv = styled.div`
   height: 13.8rem;
-  width: 600px;
-  /* background-color: #fffadb; */
+  width: 37.5rem;
   background-color: ${(prop) => prop.bgColor};
   margin-top: -5px;
   display: flex;
   align-items: center;
   justify-content: center;
-  /* border: 1px solid brown; */
 `;
 
 export const ProjectDiscCard = styled.div`
@@ -60,4 +57,11 @@ export const ProjectDiscLink = styled.p`
   color: ${(props) => props.txtColor};
   font-size: 0.9rem;
   font-family: "Poppins", sans-serif;
+`;
+
+export const ShowMoreButton = styled.a`
+  background-color: #f6f6f6;
+  margin: auto;
+  height: 2rem;
+  width: 5rem;
 `;
