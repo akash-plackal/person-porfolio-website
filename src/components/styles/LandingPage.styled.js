a97a6ed06b04d317851323d1864fd3cc47a38fde
akash plackal <akashplackal@gmail.com>
2022-02-12 08:14:18 +0530

    made navbar responsive

--- src/components/styles/LandingPage.styled.js
+++ src/components/styles/LandingPage.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { size } from "../../data/breakpoint";
 
 export const PageDiv = styled.div`
   display: flex;
@@ -13,14 +14,23 @@ export const PageDiv = styled.div`
 
 export const Title = styled.h1`
   color: #001142;
-  font-size: 2.531rem;
+  font-size: clamp(1.7rem, 234%, 4rem);
   font-weight: 500;
   letter-spacing: -1px;
   text-align: center;
+
+  /* @media (min-width: ${size.laptopL}) {
+    font-size: 3rem;
+  }
+
+  @media (max-width: ${size.mobileL}) {
+    font-size: 1.7rem;
+    padding: 2.5rem;
+  } */
 `;
 
 export const P = styled.p`
   font-weight: 400;
   color: #001142;
-  font-size: 0.769rem;
+  font-size: clamp(0.669rem, 0.769rem, 0.869rem);
 `;
