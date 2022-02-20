1b19c669ee00cccd1cb35f510ac6100fbfcbc4b7
akash plackal <akashplackal@gmail.com>
2022-02-20 07:15:32 +0530

    called aos in useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AboutMe from "./components/AboutMe";
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
@@ -6,9 +6,17 @@ import LandingPage from "./components/LandingPage";
 import NavBar from "./components/NavBar";
 import WorkSection from "./components/WorkSection";
 import Dropdown from "./components/Dropdown";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
   const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    AOS.init();
+
+    return AOS.refresh();
+  }, []);
 
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-AOS.init();
 
 ReactDOM.render(
   <React.StrictMode>
